68bb446267781b510d457d97fda6a6734a76e5d6
LH <masterX244@gmail.com>
2023-02-28 23:02:00 +0100

    working solution

--- grab_log.bat
+++ grab_log.bat
@@ -2,10 +2,11 @@
 set /p ip=Enter IP address of the Impulse Helmet: 
 echo %ip%
 echo START
-wget -q --timeout=1 --post-file prepare.dat --no-check-certificate -o - -O - https://%ip%/cgi-bin/query.cgi
-echo "PREPARED"
-wget -q --timeout=1 --post-file line1.dat --no-check-certificate -o - -O - https://%ip%/cgi-bin/query.cgi
+REM wget -q --timeout=1 --post-file prepare.dat --no-check-certificate -o - -O - https://%ip%/cgi-bin/query.cgi
+REM echo "PREPARED"
+REM wget --timeout=1 --tries=1 --post-file line1.dat --no-check-certificate -o - -O - 
+curl --max-time 10 --insecure https://%ip%/cgi-bin/query.cgi --data-binary @line1.dat 
 echo 1
-wget -q --timeout=1 --post-file exec.dat --no-check-certificate -o - -O - https://%ip%/cgi-bin/query.cgi
+curl --max-time 10 --insecure https://%ip%/cgi-bin/query.cgi --data-binary @exec.dat 
 echo exec
-wget -q --timeout=1 --no-check-certificate -o - -O - https://%ip%/cgi-bin/get_log.sh
+curl --max-time 10 --insecure https://%ip%/cgi-bin/get_log.sh
